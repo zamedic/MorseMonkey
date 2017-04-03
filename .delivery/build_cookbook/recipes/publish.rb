fdf8de2590aedd6d4c39c34577b7fe997c501563
Marc Arndt <marc@marcarndt.com>
2017-04-03 16:51:50 +0200

    adding s3 release path

--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -3,4 +3,14 @@
 # Recipe:: publish
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+secrets = get_project_secrets
+
+maven_setting 'settings.servers' do
+  value 'server' => {
+      'id' => 's3.releases',
+      'username' => secrets['s3-user'],
+      'password' => secrets['s3-key']
+  }
+end
+
 include_recipe 'coffee-truck::publish'
--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -3,4 +3,16 @@
 # Recipe:: unit
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+secrets = get_project_secrets
+
+maven_setting 'settings.servers' do
+  value 'server' => {
+      'id' => 's3.releases',
+      'username' => secrets['s3-user'],
+      'password' => secrets['s3-key']
+  }
+end
+
+
 include_recipe 'coffee-truck::unit'
